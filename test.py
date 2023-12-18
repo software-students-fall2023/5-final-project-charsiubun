374fedf589cce6399d53d5c9804266a1503d7634
Yucheng-XPH <yx2354@nyu.edu>
2023-12-18 23:14:51 +0800

    deleting testing file

--- test.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template("login.html")
-
-app.run(host="0.0.0.0", port= 50100, debug=True)
