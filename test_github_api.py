af4abcbc7d9535d2a1c99a2e1f88421711ae15b3
Yucheng-XPH <yx2354@nyu.edu>
2023-12-19 21:28:21 +0800

    test_github_api.py

--- test_github_api.py
+++ test_github_api.py
@@ -1,122 +1,100 @@
-"""
-Those are libraries for pytest (check app.py for their usage)
-"""
-import os
+# test_github_api.py
+# """
+# Those are libraries for pytest (check app.py for their usage)
+# """
 import pytest
+import unittest
+from unittest.mock import patch, MagicMock
+from github_api import (
+    get_github_user_year_contributions,
+    get_user_contributed_repos,
+    calculate_rating,
+    get_feedback,
+)
 
-os.environ["TESTING"] = "true"
-# pylint: disable=wrong-import-position
-from app import app as flask_app
+# Mocked data for testing
+username = "testuser"
+github_token = "testtoken"
 
+@pytest.fixture(name="client")
+def create_connection(test_app):
+    """Fixture to create a test client for the Flask app."""
+    return test_app.test_client()
 
-# @pytest.fixture(name="test_app")
-# def create_app():
-#     """Fixture to create a Flask app for testing."""
-#     app = flask_app
-#     return app
+@pytest.fixture(name="database")
+def creat_db(test_app):
+    """Fixture to set up and tear down a mock database for testing."""
+    with test_app.app_context():
+        db = test_app.config["MONGO_CONN"].note_app
+        db.blogs.insert_one({"title": "Test Blog Title", "main_body": "Test body", "owner":"test user"})
+        yield db
+        db.blogs.delete_one({"title": "Note Title", "owner":"test user"})
 
+def test_get_user_contributed_repos():
+    # Test the function with valid inputs
+    repos_info = get_user_contributed_repos(username, github_token)
+    assert isinstance(repos_info, dict)
 
-# @pytest.fixture(name="client")
-# def create_connection(test_app):
-#     """Fixture to create a test client for the Flask app."""
-#     return test_app.test_client()
+def test_calculate_rating():
+    # Test the function with valid inputs
+    rating_data = {
+        "Contributed Repos": {},
+        "Public Repos": 5,
+        "Year Contributions": {
+            "totalCommitContributions": 100,
+            "totalIssueContributions": 50,
+            "totalPullRequestContributions": 30,
+            "totalPullRequestReviewContributions": 20,
+        },
+        "Recent Repositories": [
+            {"Forks": 10, "Languages": {"Python": 100, "JavaScript": 50}},
+            {"Forks": 5, "Languages": {"Java": 80, "JavaScript": 30}},
+        ],
+    }
+    rating = calculate_rating(rating_data)
+    assert 0 <= rating <= 100
 
+def test_get_feedback():
+    # Test the function with valid inputs
+    feedback = get_feedback(username, 90)
+    assert isinstance(feedback, str)
 
-# @pytest.fixture(name="database")
-# def creat_db(test_app):
-#     """Fixture to set up and tear down a mock database for testing."""
-#     with test_app.app_context():
-#         db = test_app.config["MONGO_CONN"].note_app
-#         db.blogs.insert_one({"title": "Test Blog Title", "main_body": "Test body", "owner":"test user"})
-#         yield db
-#         db.blogs.delete_one({"title": "Note Title", "owner":"test user"})
+class TestGitHubAPI(unittest.TestCase):
+    @patch('requests.post')
+    def test_get_github_user_year_contributions(self, mock_post):
+        # Mocking the response from GitHub API
+        mock_response = MagicMock()
+        mock_response.status_code = 200
+        mock_response.json.return_value = {'data': {'user': {'contributionsCollection': {'totalCommitContributions': 10}}}}
+        mock_post.return_value = mock_response
 
+        # Testing the function
+        result = get_github_user_year_contributions('testuser', 'testtoken')
 
+        # Assertions
+        self.assertEqual(result['totalCommitContributions'], 10)
 
+    @patch('requests.get')
+    def test_get_user_contributed_repos(self, mock_get):
+        # Mocking the response from GitHub API
+        mock_response = MagicMock()
+        mock_response.status_code = 200
+        mock_response.json.return_value = {'items': [{'repository': {'full_name': 'test/repo'}}]}
+        mock_get.return_value = mock_response
 
-def test_get_github_user_year_contributions(username, github_token):
-    """Test to get a github user's yearly contributions."""
-    
-    assert response.status_code == 200
+        # Testing the function
+        result = get_user_contributed_repos('testuser', 'testtoken')
 
+        # Assertions
+        self.assertIn('test/repo', result)
 
+    def test_get_feedback(self):
+        # Testing the function
+        result = get_feedback('testuser', 90)
 
-# def test_show_main_screen(client):
-#     """Test showing the main screen."""
-#     response = client.get("/")
-#     assert response.status_code == 200
-#     assert b"Easy Note" in response.data
+        # Assertions
+        self.assertIsInstance(result, str)
 
-
-
-
-# def test_capture_image(client):
-#     """Test the image capture screen."""
-#     response = client.get("/capture_image")
-#     assert response.status_code == 200
-#     assert b"<title>Add Course</title>" in response.data
-
-
-# def test_upload_image(client):
-#     """Test uploading an image."""
-#     valid_base64_image = (
-#         "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAA"
-#         "HElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=="
-#     )
-#     mock_image = {"imageData": valid_base64_image}
-#     response = client.post("/upload_image", json=mock_image)
-#     assert response.status_code == 200
-
-
-# def test_show_edit_note(client, database):  # pylint: disable=unused-argument
-#     """Test showing the edit note screen."""
-#     test_title = "Note Title"
-#     response = client.get(f"/show_edit_note?title={test_title}")
-#     assert response.status_code == 200
-#     assert b"Edit Notes" in response.data
-
-
-# def test_edit_note_confirm(client):
-#     """Test confirming an edited note."""
-#     test_title = "Test Note Title"
-#     mock_note_data = {"title": "New Title", "main_body": "New Body"}
-#     response = client.post(f"/edit_confirm/{test_title}", data=mock_note_data)
-#     assert response.status_code == 200
-#     assert b"Note Updated" in response.data
-
-
-# def test_delete_note(client):
-#     """Test deleting a note."""
-#     test_title = "Note Title"
-#     response = client.get(f"/delete_note/{test_title}")
-#     assert response.status_code == 302
-
-
-# def test_show_all_notes(client, database):  # pylint: disable=unused-argument
-#     """Test showing all notes."""
-#     response = client.get("/show_all_notes")
-#     assert response.status_code == 200
-#     assert b"Note Title" in response.data
-
-
-# def test_show_search_notes(client):
-#     """Test showing the search notes screen."""
-#     response = client.get("/show_search_notes")
-#     assert response.status_code == 200
-#     assert b"Search" in response.data
-
-
-# def test_search_notes(client):
-#     """Test searching notes."""
-#     mock_search_data = {"keywords": "Test Query"}
-#     response = client.post("/search", data=mock_search_data)
-#     assert response.status_code == 200
-
-
-# def test_add_notes_post(client, database):
-    """Test adding a new note via POST request."""
-    new_note = {"title": "New Note", "main_body": "This is a new note."}
-    response = client.post("/add", data=new_note)
-    database.notes.delete_many({})
-    assert response.status_code == 200
-    os.environ.pop("TESTING", None)
+# Please add more tests based on different scenarios and edge cases.
+        
+# Run the coverage test using the following codes: pytest --cov=github_api --cov-report term-missing test_github_api.py
